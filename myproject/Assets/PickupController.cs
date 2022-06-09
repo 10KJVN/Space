0bebcd32e184baeb4f8418280c03ddedebf738c8
GaelMoney <25393@ma-web.nl>
2022-06-09 12:47:08 +0200

    Added my game with my weaponpickup for testing

---
+++ myproject/Assets/PickupController.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupController : MonoBehaviour
+{
+
+    public ProjectileGun gunScript;
+    public Rigidbody rb;
+    public BoxCollider coll;
+    public Transform player, gunContainer, fpsCam;
+
+    public float pickupRange;
+    public float dropForwardForce, dropUpwardForce;
+
+    public bool equipped;
+    public static bool slotFull;
+
+    private void Start()
+    {
+        //Setup
+        if (!equipped)
+        {
+            gunScript.enabled = false;
+            rb.isKinematic = false;
+            coll.isTrigger = false;
+        }
+        if (equipped)
+        {
+            gunScript.enabled = true;
+            rb.isKinematic = true;
+            coll.isTrigger = true;
+            slotFull = true;
+        }
+    }
+
+    private void Update()
+    {
+        //Checks if player is in range and "E" is pressed
+        Vector3 distanceToPlayer = player.position - transform.position;
+        if (!equipped && distanceToPlayer.magnitude <= pickupRange && Input.GetKeyDown(KeyCode.E) && !slotFull) pickUp();
+
+        //Drop if equipped and "Z" is pressed
+        if (equipped && Input.GetKeyDown(KeyCode.Z)) Drop();
+    }
+    private void pickUp()
+    {
+        equipped = true;
+        slotFull = true;
+
+        //Make a weapon the child of the camera and move it to a default position
+        transform.SetParent(gunContainer);
+        transform.localPosition = Vector3.zero;
+        transform.localRotation = Quaternion.Euler(Vector3.zero);
+        transform.localScale = Vector3.one;
+
+        //Make Rigidbody kinematic and BoxCollider a trigger
+        rb.isKinematic = true;
+        coll.isTrigger = true;
+
+        //Enables GunScript
+        gunScript.enabled = true;
+
+    }
+
+    private void Drop()
+    {
+        equipped = false;
+        slotFull = false;
+
+        //Set parent to null
+        transform.SetParent(null);
+
+        //Make Rigidbody kinematic and BoxCollider a trigger
+        rb.isKinematic = false;
+        coll.isTrigger = false;
+
+        //Gun carries momentum of player
+        rb.velocity = player.GetComponent<Rigidbody>().velocity;
+
+        //AddForce
+        rb.AddForce(fpsCam.forward * dropForwardForce, ForceMode.Impulse);
+        rb.AddForce(fpsCam.up * dropUpwardForce, ForceMode.Impulse);
+        //Add random rotation
+        float random = Random.Range(-1f, 1f);
+        rb.AddTorque(new Vector3(random, random, random)*10);
+
+        //Disables GunScript
+        gunScript.enabled = false;
+
+
+    }
+    
+}
---
+++ myproject/Assets/Scripts/script.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public int getal;
+    // Start is called before the first frame update
+    void Start()
+    {
+        string name = gameObject.name;//
+        Debug.Log(name);
+        Debug.Log(gameObject.transform.position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(getal);
+        Debug.LogError("Gael Griffith");
+    }
+}
